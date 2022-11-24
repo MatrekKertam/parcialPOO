7a0e970e0dc81f1f8e73d600e84361449a53888e
Matias Bernal <matrekb@gmail.com>
2022-11-24 17:08:46 -0300

    feat: add square class

---
+++ src/Square.java
@@ -0,0 +1,36 @@
+public class Square extends Rectangle {
+    public double side;
+
+    Square(){
+
+    }
+    Square(double side){
+
+    }
+    Square(double side, String color, boolean filled){
+
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    public void setWidth(double width){
+        this.width = width;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "side=" + side +
+                '}';
+    }
+}
